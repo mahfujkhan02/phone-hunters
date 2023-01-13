16a74a804e45a811ae60ae95b17d0803a9ca828d
mahfujkhan02 <mahfujtanim450@gmail.com>
2023-01-14 02:55:21 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const loadPhones = async() =>{
+    const url = ` https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+}
+
+loadPhones()
