d05896e42be7395140e2ded065ea8c92c9ab9158
mahfujkhan02 <mahfujtanim450@gmail.com>
2023-01-14 20:44:35 +0600

    added actionlistener button

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
-const loadPhones = async(searchText) =>{
+const loadPhones = async(searchText , dataLimit) =>{
     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url)
     const data = await res.json()
-    displayPhones(data.data)
+    displayPhones(data.data , dataLimit)
 }
 
-const displayPhones = phones =>{
+const displayPhones = (phones , dataLimit) =>{
    // console.log(phones)
 
    const phoneContainer = document.getElementById('phone-container')
    phoneContainer.innerText = ``;
 
 //    display 20 phones only
+          const showall = document.getElementById('show-all')
 
-   phones = phones.slice(0,20)
+        if(dataLimit && phones.length > 20){
+
+          phones = phones.slice(0,20);
+
+          showall.classList.remove('d-none')
+
+      }
+
+      else{
+
+        showall.classList.add('d-none')
+      }
+
+   
 
    // display no phone found 
 
@@ -56,15 +70,19 @@ const displayPhones = phones =>{
    toggleSpinner(false)
 }
 
+const processSerach = (dataLimit) =>{
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit)
+}
+
 
 // handle search button click
 document.getElementById('btn-serach').addEventListener('click', function(){
 
     // start loader 
-    toggleSpinner(true);
-    const searchField = document.getElementById('search-field')
-    const searchText = searchField.value;
-    loadPhones(searchText)
+           processSerach(20);
 });
 
 const toggleSpinner = isLoading =>{
@@ -82,5 +100,11 @@ const toggleSpinner = isLoading =>{
 
     
 }
+
+document.getElementById('btn-show-all').addEventListener('click', function(){
+
+     processSerach();
+
+});
 
 //loadPhones()
