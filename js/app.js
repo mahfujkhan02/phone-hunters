f3a190c4c58ac8a580f5d5895805334695fe95c1
mahfujkhan02 <mahfujtanim450@gmail.com>
2023-01-14 22:16:59 +0600

    added enter button clicked

--- js/app.js
+++ js/app.js
@@ -55,10 +55,11 @@ const displayPhones = (phones , dataLimit) =>{
     <div class="card p-4">
     <img src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${phone.phone_name}</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-  </div>
-</div>
+         <h5 class="card-title">${phone.phone_name}</h5>
+         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+         <butoon onclick = "loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+     </div>
+     </div>
     
     `;
     phoneContainer.appendChild(phoneDiv)
@@ -85,6 +86,15 @@ document.getElementById('btn-serach').addEventListener('click', function(){
            processSerach(20);
 });
 
+        // search input field enter key handler
+
+        document.getElementById('search-field').addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+              // code for enter
+              processSerach(20)
+            }
+        });
+
 const toggleSpinner = isLoading =>{
 
     const loaderSection = document.getElementById('loader')
@@ -106,5 +116,14 @@ document.getElementById('btn-show-all').addEventListener('click', function(){
      processSerach();
 
 });
+
+const loadPhoneDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.data)
+    
+
+}
 
 //loadPhones()
